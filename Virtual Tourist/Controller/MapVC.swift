02019d9de7bb9e06738276aed9fe57c0e77e1c6c
Tim McEwan <blk_hotsauce@yahoo.com>
2018-05-21 14:22:10 -0600

    reason: 'An NSManagedObject of class 'Pin' must have a valid NSEntityDescription.

--- Virtual Tourist/Controller/MapVC.swift
+++ Virtual Tourist/Controller/MapVC.swift
@@ -7,31 +7,59 @@
 //
 
 import UIKit
+import CoreData
 import Alamofire
 import MapKit
 import CoreLocation
 
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
+    var dataController:DataController!
+    
+    
+  
+
     let annotation = MKPointAnnotation()
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
-    let regionRadius: Double = 1000
+    let regionRadius: Double = 7000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
         addDoubleTap()
+        //NSFetchRequest  SELECT Query
+        let fetchRequest: NSFetchRequest<Pin> = Pin.fetchRequest()
+//        // NSSortDesciotor is like ORDER by in MYSQL
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        if let result = try? dataController.viewContext.fetch(fetchRequest){
+//            Notebooks and result if there is one there
+            PinPinSaved = result
+//            reload tableView or whatever you are using
+            mapView.reloadInputViews()
+        }
+
     }
     func addDoubleTap() {
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
-        doubleTap.numberOfTapsRequired = 2
+        //Long gesture will drop the pin
+        let doubleTap = UILongPressGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
+//        doubleTap.numberOfTapsRequired = 1
+        doubleTap.minimumPressDuration = 0.5
         doubleTap.delegate = self
         mapView.addGestureRecognizer(doubleTap)
   
     }
+    func addPin (lat: Double, long: Double) {
+        let pin = Pin(context: dataController.viewContext)
+        pin.lat = lat
+        pin.long = long
+        pin.creationDate = Date()
+        try? dataController.viewContext.save()
+    }
+
 
 }
 extension MapVC: MKMapViewDelegate{
@@ -45,11 +73,17 @@ extension MapVC: MKMapViewDelegate{
         let touchPoint = sender.location(in: mapView)
         print(touchPoint)
             let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
-        print(touchCoordinate)
+        print("This is the map gps coordinate\(touchCoordinate)")
         annotation.coordinate = CLLocationCoordinate2D(latitude: touchCoordinate.latitude , longitude: touchCoordinate.longitude)
         mapView.addAnnotation(annotation)
-        
+        //This will save the pin in coreData
+        let lat = Double(touchCoordinate.latitude)
+        let long = Double(touchCoordinate.longitude)
+        print("Printing this \(lat)")
+        addPin(lat: lat, long: long)
     }
+
+
 }
 extension MapVC: CLLocationManagerDelegate {
     func configureLocationServices() {
